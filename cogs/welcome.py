820bf1fcb46c55d7289a58ebcf983daa3e9cd29a
LMzK <73669620+L-MzK@users.noreply.github.com>
2021-07-11 21:24:03 +0200

    Direktnachricht beim Beitritt hinzugefügt

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -16,3 +16,14 @@ class Welcome(commands.Cog):
               f'Die #teeküche hier ist der "Off-topic-Channel", also für den lockeren Austausch zwischen Kommilitoninnen :dancers:\n'
               f"Viel Spaß beim Stöbern und Netzwerken!"
         await channel.send(msg)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await utils.send_dm(member,
+                            f"Willkommen auf dem Studi-Server für Kultur- und Sozialwissenschaften, schön dass du hierhergefunden hast!" \n \n
+                            f"Bei Bedarf wurden in <#{os.getenv('DISCORD_DISCORDTIPPS_CHANNEL')}> ein paar hilfreiche Infos zum Umgang mit Discord gesammelt, schau gerne rein! " \n
+                            f":books: Im Channel <#{os.getenv('DISCORD_UNITALK_CHANNEL')}> kannst du dich mit Kommilitoninnen über Themen rund um das Studium unterhalten, "
+                            f"in der <#{os.getenv('DISCORD_OFFTOPIC_CHANNEL')}> (der sogenannte Offtopic-Channel) können alle anderen Themen besprochen werden :speech_balloon:" \n \n
+                            f"Und wenn du magst, kannst du gerne etwas über dich in der <#{os.getenv('DISCORD_INTRODUCTION_CHANNEL')}> erzählen." \n \n
+                            f"Falls du bei etwas Hilfe brauchen solltest, schreib mir doch eine private Nachricht. Das Moderatoren Team wird sich dann darum kümmern."
+                            f"Mach es dir gemütlich und vorallem: zöger nicht Fragen zu stellen, falls du welche hast!" \n)      
