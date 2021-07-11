663ddf9e6ba86845543f1dca4cb4179252053451
dnns01 <git@dnns01.de>
2021-07-11 21:04:24 +0200

    Fix several things to get epikur up and running

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+import emoji
 import discord
 from discord.ext import commands
 
@@ -15,9 +16,7 @@ class Roles(commands.Cog):
         self.bot = bot
         self.roles_file = os.getenv("DISCORD_ROLES_FILE")
         self.channel_id = int(os.getenv("DISCORD_ROLLEN_CHANNEL"))
-        self.degree_program_message_id = int(os.getenv("DISCORD_DEGREE_PROGRAM_MSG"))
-        self.color_message_id = int(os.getenv("DISCORD_COLOR_MSG"))
-        self.special_message_id = int(os.getenv("DISCORD_SPECIAL_MSG"))
+        self.role_message_id = int(os.getenv("DISCORD_ROLE_MSG", "0"))
         self.assignable_roles = {}
         self.load_roles()
 
@@ -27,46 +26,10 @@ class Roles(commands.Cog):
         roles_file = open(self.roles_file, mode='r')
         self.assignable_roles = json.load(roles_file)
 
-    def get_degree_program_emojis(self):
-        """ Creates a dict for degree program role emojis """
-
-        tmp_emojis = {}
-        emojis = {}
-        degree_program_assignable = self.assignable_roles[0]
-
-        # start with getting all emojis that are used in those roles as a dict
-        for emoji in self.bot.emojis:
-            if emoji.name in degree_program_assignable:
-                tmp_emojis[emoji.name] = emoji
-
-        # bring them in desired order
-        for key in degree_program_assignable.keys():
-            emojis[key] = tmp_emojis.get(key)
-
-        return emojis
-
-    def get_color_emojis(self):
-        """ Creates a dict for color role emojis """
-
-        emojis = {}
-        color_assignable = self.assignable_roles[1]
-
-        # start with getting all emojis that are used in those roles as a dict
-        for emoji in self.bot.emojis:
-            if emoji.name in color_assignable:
-                emojis[emoji.name] = emoji
-
-        return emojis
-
-    def get_special_emojis(self):
-        """ Creates a dict for special role emojis """
-
-        return self.assignable_roles[2]
-
     def get_key(self, role):
         """ Get the key for a given role. This role is used for adding or removing a role from a user. """
 
-        for key, role_name in self.assignable_roles[0].items():
+        for key, role_name in self.assignable_roles.items():
             if role_name == role.name:
                 return key
 
@@ -107,95 +70,45 @@ class Roles(commands.Cog):
         brief="Aktualisiert die Vergabe-Nachricht von Studiengangs-Rollen.",
         mod=True
     )
-    @commands.command("update-degree-program")
+    @commands.command("update-roles")
     @commands.check(utils.is_mod)
     async def cmd_update_degree_program(self, ctx):
         channel = await self.bot.fetch_channel(self.channel_id)
-        message = await channel.fetch_message(self.degree_program_message_id)
-        degree_program_emojis = self.get_degree_program_emojis()
+        message = None if self.role_message_id == 0 else await channel.fetch_message(self.role_message_id)
 
         embed = discord.Embed(title="Vergabe von Studiengangs-Rollen",
-                              description="Durch klicken auf die entsprechende Reaktion kannst du dir die damit assoziierte Rolle zuweisen, oder entfernen. Dies funktioniert so, dass ein Klick auf die Reaktion die aktuelle Zuordnung dieser Rolle ändert. Das bedeutet, wenn du die Rolle, die mit <:St:763126549327118366> assoziiert ist, schon hast, aber die Reaktion noch nicht ausgewählt hast, dann wird dir bei einem Klick auf die Reaktion diese Rolle wieder weggenommen. ")
+                              description="Durch klicken auf die entsprechende Reaktion kannst du dir die damit assoziierte Rolle zuweisen, oder entfernen. Dies funktioniert so, dass ein Klick auf die Reaktion die aktuelle Zuordnung dieser Rolle ändert. Das bedeutet, wenn du die Rolle, die mit :regional_indicator_a: assoziiert ist, schon hast, aber die Reaktion noch nicht ausgewählt hast, dann wird dir bei einem Klick auf die Reaktion diese Rolle wieder weggenommen. ")
 
         value = f""
-        for key, emoji in degree_program_emojis.items():
+        for role_emoji, name in self.assignable_roles.items():
             if emoji:
-                value += f"<:{key}:{emoji.id}> : {self.assignable_roles[0].get(key)}\n"
+                value += f"{role_emoji} : {name}\n"
 
         embed.add_field(name="Rollen",
                         value=value,
                         inline=False)
 
-        await message.edit(content="", embed=embed)
-        await message.clear_reactions()
+        if message:
+            await message.edit(content="", embed=embed)
+            await message.clear_reactions()
+        else:
+            message = await channel.send(embed=embed)
 
-        for emoji in degree_program_emojis.values():
-            if emoji:
-                await message.add_reaction(emoji)
-
-    @help(
-        category="updater",
-        brief="Aktualisiert die Vergabe-Nachricht von Farb-Rollen.",
-        mod=True
-    )
-    @commands.command("update-color")
-    @commands.check(utils.is_mod)
-    async def cmd_update_color(self, ctx):
-        channel = await self.bot.fetch_channel(self.channel_id)
-        message = await channel.fetch_message(self.color_message_id)
-        color_emojis = self.get_color_emojis()
-
-        embed = discord.Embed(title="Vergabe von Farb-Rollen",
-                              description="Durch klicken auf die entsprechende Reaktion kannst du dir die damit assoziierte Rolle zuweisen, oder entfernen. Dies funktioniert so, dass ein Klick auf die Reaktion die aktuelle Zuordnung dieser Rolle ändert. Das bedeutet, wenn du die Rolle, die mit <:FarbeGruen:771451407916204052> assoziiert ist, schon hast, aber die Reaktion noch nicht ausgewählt hast, dann wird dir bei einem Klick auf die Reaktion diese Rolle wieder weggenommen. ")
-
-        await message.edit(content="", embed=embed)
-        await message.clear_reactions()
-
-        for emoji in color_emojis.values():
-            if emoji:
-                await message.add_reaction(emoji)
-
-    @help(
-        category="updater",
-        brief="Aktualisiert die Vergabe-Nachricht von Spezial-Rollen.",
-        mod=True
-    )
-    @commands.command("update-special")
-    @commands.check(utils.is_mod)
-    async def cmd_update_special(self, ctx):
-        channel = await self.bot.fetch_channel(self.channel_id)
-        message = await channel.fetch_message(self.special_message_id)
-        special_emojis = self.get_special_emojis()
-
-        embed = discord.Embed(title="Vergabe von Spezial-Rollen",
-                              description="Durch klicken auf die entsprechende Reaktion kannst du dir die damit assoziierte Rolle zuweisen, oder entfernen. Dies funktioniert so, dass ein Klick auf die Reaktion die aktuelle Zuordnung dieser Rolle ändert. Das bedeutet, wenn du die Rolle, die mit :exclamation: assoziiert ist, schon hast, aber die Reaktion noch nicht ausgewählt hast, dann wird dir bei einem Klick auf die Reaktion diese Rolle wieder weggenommen. ")
-
-        value = f""
-        for emoji, role in special_emojis.items():
-            value += f"{emoji} : {role}\n"
-
-        embed.add_field(name="Rollen",
-                        value=value,
-                        inline=False)
-
-        await message.edit(content="", embed=embed)
-        await message.clear_reactions()
-
-        for emoji in special_emojis.keys():
-            await message.add_reaction(emoji)
+        for key in self.assignable_roles.keys():
+            if role_emoji := emoji.EMOJI_ALIAS_UNICODE_ENGLISH.get(key):
+                await message.add_reaction(role_emoji)
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if payload.user_id == self.bot.user.id or payload.message_id not in [self.degree_program_message_id,
-                                                                             self.color_message_id,
-                                                                             self.special_message_id]:
+        if payload.user_id == self.bot.user.id or payload.message_id != self.role_message_id:
             return
 
-        if payload.emoji.name not in self.assignable_roles[0] and payload.emoji.name not in self.assignable_roles[
-            1] and payload.emoji.name not in self.assignable_roles[2]:
+        role_emoji = emoji.UNICODE_EMOJI_ALIAS_ENGLISH.get(payload.emoji.name)
+
+        if role_emoji not in self.assignable_roles:
             return
 
-        role_name = ""
+        role_name = self.assignable_roles.get(role_emoji)
         guild = await self.bot.fetch_guild(payload.guild_id)
         member = await guild.fetch_member(payload.user_id)
         channel = await self.bot.fetch_channel(payload.channel_id)
@@ -203,13 +116,6 @@ class Roles(commands.Cog):
         roles = member.roles
 
         await message.remove_reaction(payload.emoji, member)
-
-        if payload.emoji.name in self.assignable_roles[0]:
-            role_name = self.assignable_roles[0].get(payload.emoji.name)
-        elif payload.emoji.name in self.assignable_roles[1]:
-            role_name = self.assignable_roles[1].get(payload.emoji.name)
-        else:
-            role_name = self.assignable_roles[2].get(payload.emoji.name)
 
         for role in roles:
             if role.name == role_name:
--- cogs/welcome.py
+++ cogs/welcome.py
@@ -11,8 +11,8 @@ class Welcome(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         channel = await self.bot.fetch_channel(self.channel_id)
-        msg = f"Hallo {member.mention} und willkommen auf dem KSW-Studi-Server der FernUni, schön dass du hierhergefunden hast! \n"
+        msg = f"Hallo {member.mention} und willkommen auf dem KSW-Studi-Server der FernUni, schön dass du hierhergefunden hast! \n" \
               f"Wenn du unibezogene Fragen hast: zöger nicht, sie in #studium-talk zu stellen! \n" \
-              f'Die #teeküche hier ist der "Off-topic-Channel", also für den lockeren Austausch zwischen Kommilitoninnen :dancers:\n'
+              f'Die #teeküche hier ist der "Off-topic-Channel", also für den lockeren Austausch zwischen Kommilitoninnen :dancers:\n' \
               f"Viel Spaß beim Stöbern und Netzwerken!"
         await channel.send(msg)
